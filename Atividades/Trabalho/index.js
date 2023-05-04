8dd9494ea5152a215977ddfa401c27681e1733af
Luiz Fernando <105685208+dffariac@users.noreply.github.com>
2023-05-03 23:06:19 -0300

    Add files via upload

---
+++ Atividades/Trabalho/index.js
@@ -0,0 +1,288 @@
+
+const ThemeManager = {
+  switchTheme() {
+
+    let COLOR_THEME = switchThemeButton.checked ? 'dark' : 'light';
+
+    if (COLOR_THEME === 'dark') {
+      document.querySelector("body").classList.add('dark');
+      
+    } else {
+      document.querySelector("body").classList.remove('dark')
+      
+    }
+
+  },
+
+  initTheme() {
+
+    let COLOR_THEME = window.matchMedia("(prefers-color-scheme: light)").matches ? 'light' : 'dark';
+
+    if (COLOR_THEME === "dark") {
+      document.querySelector("body").classList.add('dark');
+      switchThemeButton.checked = true
+      
+    } else {
+      document.querySelector("body").classList.remove('dark')
+      switchThemeButton.checked = false
+      
+    }
+    return COLOR_THEME
+  }
+}
+
+const Modal = {
+  toggle() {
+    document.querySelector('.modal-overlay').classList.toggle('active')
+  }
+}
+
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+  },
+  set(transactions) {
+    localStorage.setItem("dev.finances:transactions",
+      JSON.stringify(transactions))
+  }
+}
+
+const Transaction = {
+  all: Storage.get(),
+
+  add(transaction) {
+    Transaction.all.push(transaction)
+
+    App.reload()
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1)
+
+    App.reload()
+  },
+
+  incomes() {
+    // Soma das entradas
+    let income = 0
+
+    Transaction.all.forEach(transaction => {
+      if (transaction.amount >= 0) {
+        income += transaction.amount
+      }
+    })
+    return income
+  },
+
+  expenses() {
+    // Somar as saidas
+    let expense = 0
+
+    Transaction.all.forEach(transaction => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount
+      }
+    })
+    return expense
+  },
+
+  total() {
+    // Entradas menos as Saidas
+    return Transaction.incomes() + Transaction.expenses()
+  }
+}
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+    tr.dataset.index = index
+    DOM.transactionsContainer.appendChild(tr)
+  },
+
+  innerHTMLTransaction(transaction, index) {
+    const CSSclass = transaction.amount >= 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+    <td class="description">${transaction.description}</td>
+    <td class="${CSSclass}">${amount}</td>
+    <td class="date">${transaction.date}</td>
+    <td class="category">${transaction.category} </td>
+    <td class ="situation">${transaction.situation} </td>
+      <td><img onclick="Transaction.remove(${index})" src="assets/minus.svg" alt="Remover Transação"></td>
+    `
+
+    return html
+  },
+
+  updateBalance() {
+    document
+      .getElementById('incomeDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.incomes())
+    document
+      .getElementById('expenseDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+      .getElementById('totalDisplay')
+      .innerHTML = Utils.formatCurrencyTotal(Transaction.total())
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = ""
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    value = String(value).replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+    return value
+
+  },
+
+  formatCurrencyTotal(value) {
+    const signal = value >= 0 ? "" : "-"
+
+    value = String(value).replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+
+    return `${signal} ${value}`
+  },
+
+  formatAmount(value) {
+    value = value * 100
+    return Math.round(value)
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split('-')
+
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+  }
+}
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+    category: document.querySelector('input#category'),
+    situation: document.querySelector('input#situation'),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+      category: Form.category.value,
+      situation: Form.situation.value
+    }
+  },
+
+  validateFields() {
+    const { description, amount, date,category, situation } = Form.getValues()
+        
+    if( description.trim() === "" || 
+        amount.trim() === "" || 
+        date.trim() === "" ||
+        category.trim() === "" ||
+        situation.trim () == "") {
+      throw new Error("Por favor, preencha todos os campos")
+    }
+
+  },
+
+  formatValues() {
+    let { description, amount, date , category , situation } = Form.getValues()
+
+    amount = Utils.formatAmount(amount)
+
+    date = Utils.formatDate(date)
+
+    return {
+           description,
+            amount,
+            date,
+            category,
+            situation
+    }
+  },
+
+  saveTransaction(transaction) {
+    Transaction.add(transaction)
+  },
+
+  clearFields() {
+       Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+        Form.category.value = ""
+        Form.situation.value = ""
+  },
+
+  submit(event) {
+    event.preventDefault()
+
+    try {
+      Form.validateFields()
+      const transaction = Form.formatValues()
+      Form.saveTransaction(transaction)
+      Form.clearFields()
+      Modal.toggle()
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+}
+
+const App = {
+
+  init() {
+
+    Transaction.all.forEach((transaction, index) => {
+      DOM.addTransaction(transaction, index)
+    })
+
+    DOM.updateBalance()
+
+    Storage.set(Transaction.all)
+
+
+  },
+
+  reload() {
+    DOM.clearTransactions()
+    App.init()
+  }
+}
+
+const switchThemeButton = document.querySelector(".checkbox");
+const imgGit = document.querySelector(".github-icon");
+const imgLkin = document.querySelector(".linkedin-icon");
+
+
+switchThemeButton.addEventListener("click", event => {
+  ThemeManager.switchTheme()
+})
+
+ThemeManager.initTheme()
+
+App.init()
+
